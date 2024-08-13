f44b34e052c200483c82a046e1be6b9bb5b35131
Alex22GTP <116815984+Alex22GTP@users.noreply.github.com>
2024-08-12 23:24:38 -0600

    agregacion del campo mensajes  en la barra admin

--- src/components/Barra.jsx
+++ src/components/Barra.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Barra.css'; // Estilo CSS para el sidebar
 import { BiMessageSquareDetail } from 'react-icons/bi';
+import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
 
 import { FaHome, FaFileAlt, FaMap, FaEnvelope, FaSignOutAlt } from 'react-icons/fa'; // Importamos los iconos de Font Awesome
 import img from "../assets/images/logo.png"; // Importamos la imagen
@@ -16,15 +17,21 @@ const Sidebar = () => {
         <li><a href="/Principal"><FaHome /> Principal</a></li>
         <li><a href="/Reporte1"><FaFileAlt /> Reporte</a></li>
         <li><a href="/Mapa"><FaMap /> Mapa General</a></li>
+        <li><a href="/Mensajes"><FaEnvelope /> Mensajes</a></li>
 
         <li>
       <a href="/Respaldo">
         <BiMessageSquareDetail /> Respaldo
       </a>
     </li>
-        <div className="sidebar-footer">
-        <button className="sidebar-menu"><FaSignOutAlt /> Salir</button>
-      </div>
+    <div className="sidebar-footer">
+      {/* Envuelve el botón en un componente Link */}
+      <Link to="/inicio" style={{ textDecoration: "none" }}>
+        <button className="sidebar-menu">
+          <FaSignOutAlt /> Salir
+        </button>
+      </Link>
+    </div>
       </ul>
      
     </div>
